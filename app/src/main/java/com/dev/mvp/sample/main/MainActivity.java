276c69777c1430426e1bed2b95997d7af27d261c
reza-dev20 <reza.dev20@gmail.com>
2022-04-30 14:58:30 +0430

    add some drawable resource and perform main layout design

--- app/src/main/java/com/dev/mvp/sample/main/MainActivity.java
+++ app/src/main/java/com/dev/mvp/sample/main/MainActivity.java
@@ -1,11 +1,10 @@
-package com.dev.mvp.sample;
+package com.dev.mvp.sample.main;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import com.dev.mvp.sample.R;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
